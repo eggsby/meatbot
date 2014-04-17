2cc0f29937b79d1b1601819db643630613f24af6
Thomas Omans <thomas.omans@gmail.com>
2014-04-17 11:49:52 -0700

    fixing unicode errs in dumbpython

--- plugins/ding.py
+++ plugins/ding.py
@@ -3,6 +3,7 @@ from util import hook
 
 @hook.command(autohelp=False)
 def ding(inp):
-    berries = 3 if randint(0, 1) else 8
+    berries = 'ε' if randint(0, 1) else 8
     shaft = '=' * randint(5, 20)
-    return "%s ~ ~ ~ Ɑ%s%s" % (inp, shaft, berries)
+    return "{} ~ ~ ~ ᗡ{}{}".format(inp, shaft, berries)
+
