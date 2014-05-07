c965067a29d9b70df92103a8e11bf63e5e26d6f5
Thomas Omans <thomas.omans@gmail.com>
2014-05-06 23:17:11 -0700

    faces by id :skull:

--- plugins/faces.py
+++ plugins/faces.py
@@ -1,12 +1,18 @@
 from util import hook, http
 from random import choice
+import re
 
 @hook.command(autohelp=False)
 def face(inp):
     url = "http://smiley.meatcub.es:1337/api/v1"
+    
+    num = re.search('\d+$', inp)
+    if num:
+        num = int(num.group(0))
+        return http.get_json("%s/faces/%s" % (url, num)).get('content', 'whoops')
 
     if inp == "help":
-        return ".face [ <tag>|tags ] - get an emoticon - thx https://github.com/kid-icarus/lysergix-api" 
+        return ".face [ < tag | id > | tags ] - get an emoticon - thx https://github.com/kid-icarus/lysergix-api" 
 
     if inp == "tags":
         return ", ".join(http.get_json("%s/tags" % url))
